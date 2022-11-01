ba0922a2a833f3f797cc1c192483d27a43cf0be0
Chinny911 <cco04290@vsc.edu>
2022-10-31 20:39:46 -0400

    initial

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,14 @@
-//Fill in this empty file
+//declare an array named words
+let words = [];
+
+//define functions search and addWord
+
+function search(word){
+    
+}
+
+function addWord(word){
+    
+}
+
+export {search, addWord}
